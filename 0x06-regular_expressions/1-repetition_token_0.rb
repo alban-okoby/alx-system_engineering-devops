cf15619d9be08e13d0cde55fa55fff46d9cc7559
alban-okoby <albanokoby225@gmail.com>
2023-10-03 19:42:35 -0700

    Repetition Token #0

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Check if the argument matches the regular expression
+def check_match(input)
+  pattern = /School/
+  if input.match?(pattern)
+    puts "School"
+  else
+    puts ""
+  end
+end
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  # Get the argument from the command line
+  argument = ARGV[0]
+  
+  # Call the check_match function with the argument
+  check_match(argument)
+end
