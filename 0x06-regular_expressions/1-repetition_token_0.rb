e93127c089d2f8e86b567f49af025b7d488b4f20
alban-okoby <albanokoby225@gmail.com>
2023-10-03 19:45:25 -0700

    SChool

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,21 +1,3 @@
 #!/usr/bin/env ruby
 # Check if the argument matches the regular expression
-def check_match(input)
-  pattern = /School/
-  if input.match?(pattern)
-    puts "School"
-  else
-    puts ""
-  end
-end
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  # Get the argument from the command line
-  argument = ARGV[0]
-  
-  # Call the check_match function with the argument
-  check_match(argument)
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
