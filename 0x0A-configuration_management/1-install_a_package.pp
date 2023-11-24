6aedc13de7788bf7500f7bf96d96d8d28f3d2e55
alban-okoby <albanokoby225@gmail.com>
2023-11-24 10:57:24 -0800

    Issu of exc cmmd

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,6 +8,7 @@ exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  provider => 'pip3',
   require => Package['python3-pip'],
 }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,8 @@
+# Using Puppet, create a manifest that kills a process named killmenow
+
 exec { 'pkill':
   command  => 'pkill killmenow',
-  provider => 'shell',
+  onlyif  => '/usr/bin/pgrep killmenow',
+  refreshonly => true,
+  provider => 'shell'
 }
