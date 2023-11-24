479e79437ade7bfce0108ce97d4464f17bd2c327
alban-okoby <albanokoby225@gmail.com>
2023-11-24 11:12:33 -0800

    Issue of installation package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
-# install flask from pip3
+# Install a package
 
-package { 'python3-pip':
-  ensure => present,
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  provider => 'pip3',
-  require => Package['python3-pip'],
-}
-
