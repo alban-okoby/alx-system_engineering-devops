c110c8a27040a84612dd5e4f1bce435dfbe34fcd
alban-okoby <albanokoby225@gmail.com>
2024-01-18 10:20:41 +0000

    Puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,11 @@
 # automated puppet
+
 exec { 'Fix wordpress site':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
-}exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
 }
+
+# exec { 'Fix wordpress site':
+#  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+#  provider => shell,
+#}
